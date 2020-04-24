cda87d1ff6da7cadd41ac81ccfbecbf3b28e937e
Kavindu8956 <sandeepa8956@gmail.com>
2020-04-24 23:44:45 +0530

    chat server complete

---
+++ src/ChatApplication/ChatServer.java
@@ -0,0 +1,92 @@
+package ChatApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashSet;
+
+public class ChatServer {
+	
+	private static final int PORT = 9001;
+	
+	private static HashSet<String> names = new HashSet<String>();
+	
+	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	
+	public static void main(String args[]) throws Exception{
+		
+		
+	}
+	
+	private static class Handler implements Runnable {
+	
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+		
+		public Handler(Socket socket) {
+			this.socket = socket;
+			// TODO Auto-generated constructor stub
+		}
+
+		public void run() {
+
+			try {
+				
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				out = new PrintWriter(socket.getOutputStream(),true);
+				
+				while(true) {
+					out.println("SUBMINAME");
+					name = in.readLine();
+					
+					if(name == null) {
+						return;						
+					}
+					if(!names.contains(names)) {
+						names.add(name);
+						break;
+					}
+				}
+				
+				out.println("NAMEACCEPTED");
+				writers.add(out);
+				
+				while(true) {
+					
+					String input = in.readLine();
+					if(input == null) {
+						return;
+					}
+					for(PrintWriter writer : writers) {
+						writer.println("MESSAGE " + name + ": " + input);
+					}
+				}
+				
+			} catch (IOException e) {
+				
+				System.out.println(e);
+				
+			} finally {
+				
+				if(names != null) {
+					names.remove(name);
+				}
+				if(out != null) {
+					writers.remove(out);
+				}
+				try {
+					socket.close();
+				} catch (IOException e) {
+					
+				}
+			}
+			
+			
+		}
+	}
+}
